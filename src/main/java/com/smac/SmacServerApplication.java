e2556075af6ff8cc6f18f268464dd87e9db2aea1
anuragmadrewar <anuragmadrewar206@gmail.com>
2020-05-19 23:56:02 +0530

    inital commit

---
+++ src/main/java/com/smac/SmacServerApplication.java
@@ -0,0 +1,13 @@
+package com.smac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmacServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmacServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smac/demo/SmacServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smac.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmacServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
